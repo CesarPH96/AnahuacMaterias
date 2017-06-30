450948d10563729043a61d4f46273ada5f5f6f03
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-06-30 18:55:46 -0500

    Remove Semesters from seeds.js

--- seeds.js
+++ seeds.js
@@ -1,74 +1,55 @@
 var mongoose = require("mongoose");
-var Semester = require("./models/semester");
 var Course = require("./models/course");
 var Metric = require("./models/metric");
 
-var data = [
+var dataCRS = [
   {
-    name: "Semestre 1"
+    name: "Sistemas Operativos"
   },
   {
-    name: "Semestre 2"
+    name: "Estructura de Datos"
   },
   {
-    name: "Semestre 3"
+    name: "Redes Avanzadas"
+  },
+  {
+    name: "Estadistica Inferencial"
   }
 ];
 var porc = [30, 40, 30];
 function seedDB(){
   //remove all campgrounds
-  Semester.remove({}, function(err){
-    if(err){
-      console.log(err);
-    }
-    console.log("removed semesters");
     //add a few campgrounds
-    data.forEach(function(seed, i){
-      Semester.create(seed, function(err, semester){
-        if(err){
-          console.log(err);
-        }else{
-          // console.log("added a Semester");
-          //crete comment
+    dataCRS.forEach(function(crs, i){
           Course.remove({}, function(err){
-            Course.create({
-                            name: "Sistemas Operativos",
-                          }, function(err, course){
+            Course.create(crs, function(err, course){
                             if(err){
                               console.log(err);
                             }else{
                               Metric.remove({}, function(err){
                                 Metric.create({
-                                                name: "Examen "+(i+1),
-                                                porcentage: porc[i],
+                                                name: "Examen 1",
+                                                porcentage: "10",
                                                 grade: parseInt(Math.random()*10+1)
                                               }, function(err, met){
                                                 if(err){
                                                   console.log(err);
                                                 }else{
-                                                  course.metric.push(met);
+                                                  course.metrics.push(met);
                                                   course.save();
                                                   // console.log("course: "+ course);
                                                   // console.log("metric: "+met);
-                                                  // console.log("semester: " + semester);
                                                   // console.log("Created a new metric");
                                                 }
 
                                               });
-                                              semester.courses.push(course);
-                                              semester.save();
-                                              // console.log(JSON.stringify(semester.courses, null, 4));
-                                  // console.log("Created a new course");
                               });
                             }
 
             });
 
         });
-      }
       });
-    });
-  });
 }
 
 
