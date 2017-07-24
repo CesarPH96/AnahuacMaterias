7013e123b337898a26c0363bd8a92bd807b99eed
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-07-24 18:08:21 -0500

    Add function to create courses

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,42 @@
 var express       = require("express"),
     router        = express.Router(),
-    Semester      = require("../models/semester");
     Course      = require("../models/course");
-    Metric      = require("../models/metric");
 
 //configuracion de la pagina principal
 router.get("/", function(req, res){
   res.redirect("/courses");
 });
+
 router.get("/courses", function(req, res){
-  Course.find({}).populate("metrics").exec(function(err, foundCourse){
+  Course.find({},function(err, foundCourse){
         res.render("home", {courses: foundCourse});
   });
 });
+
 router.post("/courses", function(req, res){
-  console.log(req.body.metric);
+  let newMetrics = [];
+  for(let i=0; i < req.body.metrics.name.length; i++){
+    newMetrics.push({
+      name: req.body.metrics.name[i],
+      porcentage: req.body.metrics.porcentage[i],
+      grade: req.body.metrics.grade[i]
+    });
+  }
+  let newCourse = {
+    name: req.body.course.title,
+    credits: req.body.course.credits,
+    metrics: newMetrics
+  };
+
+  Course.create(newCourse, function(err, course){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/courses");
+    }
+  });
 });
+
 router.get("/courses/new", function(req, res){
   res.render("courses/new");
 });
