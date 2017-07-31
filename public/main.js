5f91ca43c13fc9305623701d57dcdcf3868cc4c0
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-07-31 18:04:09 -0500

    Courses page final touches
    - Add final grade of the semester table
    - Add and create calculateGrade, calculateFinalGrade, UpdateCourse functions
    - Move edit, delete, save below the course title
    - Restyle page title

--- public/main.js
+++ public/main.js
@@ -17,7 +17,8 @@
 
 var form = document.querySelector("#metricForm"),
     table = document.querySelector(".table"),
-    last = 6;
+    metrics = document.querySelectorAll(".metric");
+    last = metrics.length;
 
 function createForm(){
   let containerCourse = document.querySelector(".container-course");
@@ -32,7 +33,7 @@ function createForm(){
 
 function addMetric(){
   let nameMetric = document.querySelector("#nameMetric"),
-      row = table.insertRow(last++),
+      row = table.insertRow(++last),
       name = row.insertCell(0),
       porcentage = row.insertCell(1),
       grade = row.insertCell(2);
@@ -44,21 +45,21 @@ function addMetric(){
 }
 
 function deleteLastMetric(){
-  if (last > 2) {
-    table.deleteRow(--last);
+  if (last > 1) {
+    table.deleteRow(last--);
   }
 }
 
 var finalGrade = document.querySelectorAll(".final-grade-grade"),
     metricGrade = document.querySelectorAll(".metric-grade");
     updateCourse = document.querySelectorAll(".updateCourse");
-
+calculateGrade();
 function calculateGrade(){
   finalGrade.forEach(function(grade){
     let fgrade = 0;
     let childTable = grade.offsetParent.childNodes[3].childNodes;
-    for(let i = 1; i<childTable.length-2; i+=2){
-      fgrade += Number(childTable[i].childNodes[3].textContent) * (Number(childTable[i].childNodes[5].firstChild.value)/100);
+    for(let i = 3; i<childTable.length-2; i+=2){
+      fgrade += Number(childTable[i].childNodes[3].firstChild.value) * (Number(childTable[i].childNodes[5].firstChild.value)/100);
     }
     grade.innerText = parseFloat(fgrade).toFixed(2);
     console.log(fgrade);
@@ -70,6 +71,38 @@ metricGrade.forEach(function(grade, i){
   grade = grade.firstChild;
   grade.addEventListener("change", function(event){
     calculateGrade();
-
+    updateFinalGrade();
   });
 });
+var coursesFinalGrade = document.querySelector(".courses-final-grade-grade"),
+coursesGrade =  document.querySelectorAll(".courses-grade"),
+totalCredits = document.querySelector(".courses-total-credits");
+
+calculateFinalGrade();
+function calculateFinalGrade(){
+    let fGrade = 0, tCredits = 0;
+    let childTable = coursesFinalGrade.offsetParent.childNodes[3].childNodes;
+    for(let i = 1; i<childTable.length-2; i+=2){
+      fGrade += Number(childTable[i].childNodes[3].textContent) * (childTable[i].childNodes[5].textContent);
+      tCredits += Number(childTable[i].childNodes[3].textContent);
+    }
+    fGrade /= tCredits;
+    totalCredits.innerText = tCredits;
+    coursesFinalGrade.innerText = parseFloat(fGrade).toFixed(2);
+    console.log(fGrade);
+}
+
+function updateFinalGrade(){
+  coursesGrade.forEach(function(cGrade,i){
+    cGrade.textContent = finalGrade[i].textContent;
+  });
+  calculateFinalGrade();
+}
+
+function undisabled(){
+  let disable = document.querySelectorAll(".disable");
+  disable.forEach(function(dis){
+    dis.disabled = false;
+  });
+  setTimeout(function(){document.getElementById('saveChanges').submit();}, 3000);
+}
