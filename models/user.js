ffb8a8a2adbfd492087a9c93428dfd4730eee94f
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-08-03 19:55:54 -0500

    Add User model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose =  require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  courses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Courses"
+  }]
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", userSchema);
