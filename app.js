0e4c6fa3f623731b3c22c395a031a59ca28a8d6f
César Palazuelos Higuera <cesar_96_ph@hotmail.com>
2017-08-03 19:56:59 -0500

    Add passport.js functions to app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express         = require("express"),
     bodyParser      = require("body-parser"),
     app             = express(),
     seedDB          = require("./seeds"),
-    methodOverride  = require("method-override");
-
+    methodOverride  = require("method-override"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User          = require("./models/user");
 //configuración base de datos
 mongoose.connect("mongodb://localhost/Anahuac_materias");
 mongoose.Promise = global.Promise;
@@ -19,6 +21,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 // seedDB
+// configuracion de Passport
+app.use(require("express-session")({
+  secret: "Este es un codigo secreto",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
 //coneccion con las rutas
 app.use('/', indexRoutes);
 
